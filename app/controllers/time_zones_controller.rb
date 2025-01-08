f0b517877248f79258ef5c31a1f0e910a4295518
Nico Shanstrom <nicoshanstrom@NicoShantii.localdomain>
2025-01-08 13:52:40 -0500

    Removes html responses for only json

--- app/controllers/time_zones_controller.rb
+++ app/controllers/time_zones_controller.rb
@@ -3,15 +3,9 @@ class TimeZonesController < ApplicationController
 
   def set_time_zone
     if params[:time_zone].present? && current_user.update(time_zone: params[:time_zone])
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: "Time zone updated successfully!" }
-        format.json { render json: { status: "success" } }
-      end
+      render json: { status: "success" }
     else
-      respond_to do |format|
-        format.html { redirect_to root_path, alert: "Failed to update time zone." }
-        format.json { render json: { status: "error", message: "Invalid time zone" }, status: :unprocessable_entity }
-      end
+      render json: { status: "error", message: "Invalid time zone" }, status: :unprocessable_entity
     end
   end
 end
