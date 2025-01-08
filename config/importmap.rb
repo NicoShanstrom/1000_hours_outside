aa835cc4093e601c1adc5fdbfc0c9c5f54cf412e
Nico Shanstrom <nicoshanstrom@NicoShantii.localdomain>
2025-01-08 14:46:16 -0500

    explicitely loads flash_controller in importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,4 +3,5 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "controllers/flash_controller", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
