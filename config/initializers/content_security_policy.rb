3236aa981fe6f2ea3e07d3684bcf6a1f0cb6449f
Nico Shanstrom <nicoshanstrom@NicoShantii.localdomain>
2025-01-08 16:27:32 -0500

    Still works, modified csp file based on console errors in chrome inspect on desktop

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,16 +10,16 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src :self, "https://one000-outside-hours.onrender.com"
-    policy.style_src :self, :unsafe_inline, :https
-    # Specify URI for violation reports
+    policy.script_src  :self, "https://one000-outside-hours.onrender.com", :unsafe_inline
+    policy.style_src   :self, :unsafe_inline, :https
+    # Specify URI for violation reports (optional)
     # policy.report_uri "/csp-violation-report-endpoint"
   end
 
   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
   config.content_security_policy_nonce_directives = %w(script-src style-src)
 
-  # Report violations without enforcing the policy.
+  # Report violations without enforcing the policy (optional).
   # config.content_security_policy_report_only = true
 end
