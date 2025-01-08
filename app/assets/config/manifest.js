2725354192b72b359d9eea097aa607054711ff41
Nico Shanstrom <nicoshanstrom@NicoShantii.localdomain>
2025-01-08 15:09:24 -0500

    Modifies flash controller and manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,4 @@
-//= link_tree ../images
 //= link application.js
-//= link controllers/application.js
 //= link controllers/index.js
+//= link controllers/flash_controller.js
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,21 +2,9 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    // Add fade-out class after a delay
     setTimeout(() => {
-      this.element.classList.add("fade-out");
-
-      // Listen for the 'transitionend' event to remove the element
-      this.element.addEventListener("transitionend", () => {
-        this.element.remove();
-      });
-
-      // Fallback for devices where transitionend might not fire
-      setTimeout(() => {
-        if (this.element.parentNode) {
-          this.element.remove();
-        }
-      }, 1000); // Fallback removal after 1 second
+      this.element.classList.add("fade-out"); // Add fade-out class
+      setTimeout(() => this.element.remove(), 1000); // Remove after animation
     }, 5000); // Delay fade-out by 5 seconds
   }
 }
